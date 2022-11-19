187c1961ea9a7c8345b030ce483c5d857e1b80a4
hakand41 <degerhakan41@gmail.com>
2022-11-19 18:16:15 +0300

    adminozellestirme-ilktemplate

--- projeler/admin.py
+++ projeler/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from .models import Projeler
 # Register your models here.
 
-admin.site.register(Projeler)
+@admin.register(Projeler)
+class ProjelerAdmin(admin.ModelAdmin):
+    list_display = ["author", "release_date"]
+    list_display_links = ["release_date"]
+    search_fields = ["title"]
+    list_filter = ["release_date"]
+    class Meta:
+        model = Projeler
--- projeler/models.py
+++ projeler/models.py
@@ -3,8 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Projeler(models.Model):
-    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
-    title = models.CharField(max_length=50)
-    content = models.TextField()
-    proje = models.FileField()
-    release_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE, verbose_name="Proje Sahibi")
+    title = models.CharField(max_length=50, verbose_name="Başlık")
+    content = models.TextField(verbose_name="Proje Tanımı")
+    proje = models.FileField(verbose_name="Proje Dosyası")
+    release_date = models.DateTimeField(auto_now_add=True, verbose_name="Oluşturulma Tarihi")
+    def __str__(self):
+        return self.title
