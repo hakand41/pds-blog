69386ff2ddceaabf10f9e22f3cac99cd976ac8cb
hakand41 <degerhakan41@gmail.com>
2022-11-20 11:07:29 +0300

    miras-anasayfa

--- projeler/views.py
+++ projeler/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, "projeler/index.html")
+    return render(request, "index.html")
