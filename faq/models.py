e781b0dd3b09c212d88d4e29436e83550b23105a
hakand41 <degerhakan41@gmail.com>
2022-12-25 17:39:17 +0300

    models-duzenlemesi

--- faq/models.py
+++ faq/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from user.models import User
 from django.forms import ModelForm
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -9,9 +9,8 @@ class Faq(models.Model):
         ('True', 'Evet'),
         ('False', 'Hayır'),
     )
-    id = models.ForeignKey()
-    question = models.TextField(verbose_name="Soru")
-    answer = models.TextField(verbose_name="Cevap", blank=True)
+    question = models.CharField(max_lenghth=50, blank=True)
+    answer = models.CharField(max_lenghth=50, blank=True)
     status = models.CharField(choices=STATUS, verbose_name="Durum")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Oluşturulma Tarihi")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Son Güncellenme Tarihi")
@@ -22,16 +21,13 @@ class Message(models.Model):
         ('False', 'Hayır'),
         ('New', 'Yeni'),
     )
-    id = models.ForeignKey()
-    name = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, default='Yeni')
-    email = User.email
-    phone = models.CharField(max_length=15, verbose_name="Telefon Numarası")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, default='Yeni')
     subject = models.CharField(max_length=50)
     message = models.TextField(max_length=300)
     status = models.CharField(max_length=10, choices=STATUS, blank=True)
     ip = models.CharField(max_length=20, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Oluşturulma Tarihi")
-    updated_at = models.DateTimeField(auto_now=True, verbose_name="Son Güncellenme Tarihi")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class CommentForm(ModelForm):
--- home/models.py
+++ home/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -7,13 +8,12 @@ class Setting(models.Model):
         ('True', 'Evet'),
         ('False', 'Hayır'),
     )
-    id = models.ForeignKey()
     title = models.CharField(max_length=50)
-    keywords = models.CharField(max_lenghth=50, verbose_name="Anahtar Kelimeler")
-    description = models.TextField()
-    company = models.TextField()
-    adress = models.TextField(blank=True)
-    fax = models.TextField(blank=True, max_length=50)
+    keywords = models.CharField(max_lenghth=50)
+    description = models.CharField(max_lenghth=500)
+    company = models.CharField(max_lenghth=50)
+    adress = models.CharField(max_lenghth=150, blank=True)
+    fax = models.CharField(blank=True, max_length=50)
     email = models.CharField(blank=True, max_length=50)
     smtpserver = models.CharField(blank=True, max_length=50)
     smtpemail = models.CharField(blank=True, max_length=50)
@@ -21,9 +21,9 @@ class Setting(models.Model):
     facebook = models.CharField(blank=True, max_length=50)
     instagram = models.CharField(blank=True, max_length=50)
     twitter = models.CharField(blank=True, max_length=50)
-    aboutus = models.TextField()
-    contact = models.TextField()
-    references = models.TextField()
+    aboutus = models.CharField(max_lenghth=50, blank=True)
+    contact = models.CharField(max_lenghth=50, blank=True)
+    references = models.CharField(max_lenghth=50, blank=True)
     status = models.CharField(max_length=10, choices=STATUS)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
