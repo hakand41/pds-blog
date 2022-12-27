47db7bbb0cbadf2723ce02e2a6d57d3aecfe81ad
hakand41 <degerhakan41@gmail.com>
2022-12-27 13:41:04 +0300

    homepage-degisikligi

--- project/views.py
+++ project/views.py
@@ -5,7 +5,11 @@ from django.contrib import messages
 from .models import Projeler, Comment
 # Create your views here.
 def index(request):
-    return render(request, "index.html") 
+    projects = Projeler.objects.all()
+    context = {
+        "projects":projects
+    }
+    return render(request, "index.html", context) 
 
 def about(request):
     return render(request, "about.html") 
